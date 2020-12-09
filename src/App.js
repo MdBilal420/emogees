cf9c707c04d61fadee7d937e9adf1c67405f712d
MdBilal420 <mohdbilal16@gmail.com>
2020-12-09 16:09:18 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
+
 function App() {
+  const headingText = "Cool";
+  
+  var [emoji,setEmoji] = useState("");
+  var [text,setText] = useState("meaning");
+
+
+
+  
+
+  const emodict = {
+    "🥶" : "Cold Face",
+    "🏂" : "Snowboarder",
+    "🌲" : "Evergreen Tree",
+    "🏔️" : "Snow-Capped Mountain",
+    "🌁" : "Foggy",
+    "🌬️" : "Wind Face",
+    "❄️" : "Snowflake",
+    "☃️" : "Snowman",
+    "🎿" : "Skis",
+    "🧣" : "Scarf",
+    "🌂" : "Closed Umbrella",
+    "☔" : "Umbrella with Rain Drops",
+    "🧥" : "Coat",
+  }
+  
+  const emoarr = Object.keys(emodict);
+
+  const inputHandler = (e) =>{
+    const inputEmoji = e.target.value;
+    setEmoji(inputEmoji)
+
+    if(inputEmoji in emodict){
+      setText(emodict[inputEmoji]);    
+    }else{
+      setText("")
+    }
+    
+  }
+
+  const emojiHandler = (inputText) =>{
+    setText(emodict[inputText]);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <div className="container"> 
+        <h1> <span style={{backgroundColor:"lightblue",color:"black"}}>{headingText}</span> Emojis</h1>
+        <br />
+        <h1>winter is coming</h1>
+        
+        <div className="container container-center">
+          <input placeholder="Emoji" onChange={inputHandler} />
+        
+          
+          <h1 style={{size:"4rem"}}>
+          {
+            emoarr.map((emoji) => (
+              <span id="emo" onClick={()=>emojiHandler(emoji)}>
+                  {emoji}
+              </span>
+            ))
+          }
+          </h1>
+          
+
+          <div className="output">
+            <h2>{text}</h2>
+          </div>
+          
+        </div>
+        
+      </div>
     </div>
   );
 }
